18fe7bd48170436b1cf0f00d149cabd1adbcb48b
ExtranoDev <jeomade19@gmail.com>
2022-11-01 11:34:22 +0000

    puppet nginx install configuration

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,30 @@
+# nginx configuration
+
+exec {'exec':
+  command  => 'sudo apt-get -y update',
+  provider => 'shell'
+}
+
+package {'nginx':
+  ensure => 'installed'
+}
+
+file {'index.html':
+  path    => '/var/www/html/index.html'
+  owner   => 'root',
+  content => 'Hello World!\n'
+}
+
+$rdr_str = "\\\trewrite ^/redirect_me https://github.com/ExtranoDev permanent;"
+
+exec {'redirect':
+  user     => 'root',
+  command  => "sed -i '51i ${rdr_str}' /etc/nginx/sites-available/default",
+  provider => 'shell'
+}
+
+exec {'nginx_restart':
+  user     => 'root',
+  command  => 'service nginx start',
+  provider => 'shell'
+}
